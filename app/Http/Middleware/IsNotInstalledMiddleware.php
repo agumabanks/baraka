b1ef1d9f918571f09e2b659f59a78b911aa65907
agumabanks <agumabanksibrahim@gmail.com>
2025-11-04 23:41:04 +0100

    fix: Ensure APP_INSTALLED is set in .env and streamline IsNotInstalledMiddleware

--- app/Http/Middleware/IsNotInstalledMiddleware.php
+++ app/Http/Middleware/IsNotInstalledMiddleware.php
@@ -18,12 +18,7 @@ class IsNotInstalledMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-
-        try {
-            DB::connection()->getPdo();
-        } catch (\Throwable $th) {
-            return response()->view('installer.index');
-        }
+        DB::connection()->getPdo();
 
         if (Config::get('app.app_installed') == 'yes' && Schema::hasTable('settings') && Schema::hasTable('general_settings') && Schema::hasTable('users')) {
             return redirect('/');
